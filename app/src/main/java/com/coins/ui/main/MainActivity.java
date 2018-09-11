10aa6aeff462f756cd33fb3ff993e78e31506739
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-09-11 10:57:21 +0100

    refactor: attach fragment to activity

--- app/src/main/java/com/coins/ui/main/MainActivity.java
+++ app/src/main/java/com/coins/ui/main/MainActivity.java
@@ -12,6 +12,16 @@ public class MainActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // remove activity's background theme
+        getWindow().setBackgroundDrawable(null);
+
+        // create and attach fragment to activity
+        MainFragment fragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 
 }
