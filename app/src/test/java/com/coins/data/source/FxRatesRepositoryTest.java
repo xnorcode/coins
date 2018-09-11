41bbe9f258da3e5bf3d94bc9890be55a9c8b6f72
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-09-11 10:46:07 +0100

    refactor: created rates repository test

---
+++ app/src/test/java/com/coins/data/source/FxRatesRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.coins.data.source;
+
+import com.coins.data.FxRates;
+import com.coins.data.source.remote.FxRatesApi;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.reactivex.subscribers.TestSubscriber;
+
+/**
+ * Created by xnorcode on 09/09/2018.
+ */
+public class FxRatesRepositoryTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    private FxRates mRates;
+
+    @Mock
+    private FxRatesApi mRemoteDataSource;
+
+    @InjectMocks
+    private FxRatesRepository mRatesRepository;
+
+
+    @Before
+    public void setup() {
+        // init rates
+        Map<String, Double> rates = new HashMap<>();
+        rates.put("USD", 1.05);
+        mRates = new FxRates("EUR", "today", rates);
+    }
+
+
+    @Test
+    public void getLatestFxRates() {
+        // make data available in remote data source
+        Mockito.when(mRemoteDataSource.getLatestFxRates("EUR")).thenReturn(mRates);
+
+        // call method and subscribe
+        TestSubscriber<FxRates> testSubscriber = new TestSubscriber<>();
+        mRatesRepository.getLatestFxRates("EUR").subscribe(testSubscriber);
+
+        // verify remote data source method called once
+        Mockito.verify(mRemoteDataSource).getLatestFxRates("EUR");
+
+        // check results
+        testSubscriber.assertValue(mRates);
+    }
+}
