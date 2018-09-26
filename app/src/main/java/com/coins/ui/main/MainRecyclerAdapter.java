af752250ebecfb4bde764968681383a7891188a3
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-09-26 22:34:03 +0100

    refactor: edit recycler adapter

--- app/src/main/java/com/coins/ui/main/MainRecyclerAdapter.java
+++ app/src/main/java/com/coins/ui/main/MainRecyclerAdapter.java
@@ -3,7 +3,6 @@ package com.coins.ui.main;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import com.coins.R;
@@ -133,8 +132,8 @@ public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapte
     @NonNull
     @Override
     public MainRecyclerAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_list_item, parent, false);
-        return new MainRecyclerAdapterViewHolder(view);
+        return new MainRecyclerAdapterViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.main_list_item, parent, false));
     }
 
 
