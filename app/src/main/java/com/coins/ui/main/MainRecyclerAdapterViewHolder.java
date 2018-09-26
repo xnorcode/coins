3b4d8367090b23e06b0e34ea5abfc0106502d54d
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-09-27 00:02:49 +0100

    refactor: set data of RecyclerView Row Item

--- app/src/main/java/com/coins/ui/main/MainRecyclerAdapterViewHolder.java
+++ app/src/main/java/com/coins/ui/main/MainRecyclerAdapterViewHolder.java
@@ -1,5 +1,6 @@
 package com.coins.ui.main;
 
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
@@ -7,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.coins.R;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,7 +16,7 @@ import butterknife.ButterKnife;
 /**
  * Created by xnorcode on 26/09/2018.
  */
-public class MainRecyclerAdapterViewHolder extends RecyclerView.ViewHolder {
+public class MainRecyclerAdapterViewHolder extends RecyclerView.ViewHolder implements MainContract.RecyclerRowView {
 
     @BindView(R.id.list_item_icon)
     ImageView mIcon;
@@ -28,10 +30,54 @@ public class MainRecyclerAdapterViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.list_item_rate)
     EditText mRate;
 
+    private String mPackageName;
+
+    private Resources mResources;
+
+
     public MainRecyclerAdapterViewHolder(View itemView) {
         super(itemView);
 
+        // Inject Views
         ButterKnife.bind(this, itemView);
+
+        // Get package name
+        mPackageName = itemView.getContext().getApplicationContext().getPackageName();
+
+        // Get ref to resources
+        mResources = itemView.getResources();
     }
 
+    @Override
+    public void setIcon(String iconName) {
+        // TODO: 27/09/2018 include below code in presenter method setIcon()
+        // named the TRY icon resource as "turkey" to avoid system resource conflicts
+        // Add the below line into presenter logic before passing the iconName to the view
+        // !name.equals("TRY") ? name.toLowerCase() : "turkey"
+        Picasso.get()
+                .load(mResources.getIdentifier(iconName, "drawable", mPackageName))
+                .into(mIcon);
+    }
+
+    @Override
+    public void setName(String name) {
+        mName.setText(name);
+    }
+
+    @Override
+    public void setDescription(String name) {
+        // get the resource id
+        int resourceId = mResources.getIdentifier(name, "string", mPackageName);
+
+        // get the text from the string resource
+        String description = mResources.getString(resourceId);
+
+        // set description
+        mDescription.setText(description);
+    }
+
+    @Override
+    public void setRate(String rate) {
+        mRate.setText(rate);
+    }
 }
